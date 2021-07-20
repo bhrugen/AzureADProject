ee05ac9ef77964d435855dbe458ab58a5432a318
Bhrugen Patel <bhrugen1990@gmail.com>
2021-07-20 13:25:51 -0500

    Section 5 - User Service Interface

---
+++ AzureADB2CWeb/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using AzureADB2CWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureADB2CWeb.Services
+{
+    public interface IUserService
+    {
+        User Create(User user);
+        User GetById(string b2cObjectId);
+        Task<string> GetB2CTokenAsync();
+        User GetUserFromSession();
+    }
+}
---
+++ AzureADB2CWeb/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using AzureADB2CWeb.Data;
+using AzureADB2CWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureADB2CWeb.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public User Create(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetB2CTokenAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(string b2cObjectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserFromSession()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
