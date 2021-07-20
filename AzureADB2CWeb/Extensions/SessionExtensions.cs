ba9ac226706eee279772ac95edcc79331141bd39
Bhrugen Patel <bhrugen1990@gmail.com>
2021-07-20 12:10:08 -0500

    Section 5 - Session Extension

---
+++ AzureADB2CWeb/Extensions/SessionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureADB2CWeb.Extensions
+{
+    public static class SessionExtensions
+    {
+        public static T GetComplexData<T> (this ISession session, string key)
+        {
+            var data = session.GetString(key);
+            if (data == null)
+            {
+                return default(T);
+            }
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+        public static void SetComplexData<T>(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+    }
+}
